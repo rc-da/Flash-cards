bc008b2d51c8d84c30dc9b6cc148bf15b53236f4
dhinesh <aadityadhinesh@gmail.com>
2023-06-01 23:36:43 +0530

    addded the bt

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from tkinter import *
 from tkinter import messagebox
 import json
 
+
+FONT = ('TimesNewRoman', 16)
+collec_of_title = []
+all_labels = []
+all_bts = []
+cols = None
 card_title_input = None
 card_cont_input = None
-collec_of_title = []
-FONT = ('TimesNewRoman', 16)
+rows = None
+left_scroll = None
+right_scroll = None
+card_label = None
+collec_len = None
+show_no = None
+left_no = None
+right_no = None
+
 
 # ui
 window = Tk()
@@ -24,7 +37,6 @@ def add_to_file():
     collec_of_title .append(collec_title)
 
     card_data = {
-                'all_collection' : collec_of_title,
                  collec_title : [{ 'title' : card_title, 'content' : card_cont}]
                 }
 
@@ -33,7 +45,6 @@ def add_to_file():
             data = json.load(file)
             
             if collec_title in data:
-                data['all_collection'].extend(card_data['all_collection'])
                 data[collec_title].extend(card_data[collec_title])
 
             else:
@@ -53,7 +64,13 @@ def add_to_file():
 
 # to create cards
 def create_card():
-    global collec_input, card_cont_input, card_title_input
+    global collec_input, card_cont_input, card_title_input, all_bts, all_labels
+
+    for i in all_labels:
+        i.grid_forget()
+
+    for j in all_bts:
+        j.grid_forget()
 
     canvas.grid_forget()
     
@@ -66,41 +83,165 @@ def create_card():
 
     # collection name
     collection = Label(text='Collection : ', font=FONT)
-    collection.grid(column=0, row=1)
+    collection.grid(column=0, row=1, pady=15)
+    all_labels.append(collection)
     collec_input = Entry(width=20)
     collec_input.focus()
-    collec_input.grid(column=1, row=1)
+    collec_input.grid(column=1, row=1, pady=15)
+    all_labels.append(collec_input)
 
     # input cards
     card_title = Label(text='Title : ', font=FONT)
-    card_title.grid(column=0, row=2)
+    card_title.grid(column=0, row=2, pady=15)
+    all_labels.append(card_title) 
     card_title_input = Entry(width=30)
-    card_title_input.grid(column=1, row=2)
+    card_title_input.grid(column=1, row=2, pady=15)
+    all_labels.append(card_title_input)
 
     card_cont = Label(text='Content : ', font=FONT)
     card_cont.grid(column=0, row=3)
+    all_labels.append(card_cont) 
     card_cont_input = Text(width=25, height=8)
-    card_cont_input.grid(column=1, row=3)
+    card_cont_input.grid(column=1, row=3, pady=15)
+    all_labels.append(card_cont_input)
     
     add_bt = Button(text='Add', font=FONT, command=add_to_file)
     add_bt.config(padx=30)
-    add_bt.grid(column=1, row=4)
-
+    add_bt.grid(column=1, row=4, padx=10, pady=15)
+    all_bts.append(add_bt)
 
 #existing bt
 def view_existing():
-    canvas.grid_forget()
-    window.config(padx=30,pady=0)
-    title['font'] = ('TimesNewRoman', 16)
-    title.grid(column=0, row=0)
-    new_bt.grid(column=1, row=0)
-    existing_bt.grid(column=2, row=0)
+    global all_bts, all_labels, cols, rows, show_no, left_no, right_no
+    right_no = 1
+    left_no = 0
+    show_no = 0
+    cols = -1
+    rows = 1
 
     try:
+        for i in all_labels:
+            i.grid_forget()
+
+        for j in all_bts:
+            j.grid_forget()
+
+        canvas.grid_forget()
+        window.config(padx=30, pady=0)
+        title['font'] = ('TimesNewRoman', 16)
+        
+        title.grid(column=0, row=0)
+        new_bt.grid(column=1, row=0, pady=5)
+        existing_bt.grid(column=2, row=0)
+
         with open('cards.json', 'r') as file:
             data = json.load(file)
-            collection = data['all_collection']
-            print(collection)
+            total_collection = len(data.keys()) 
+            all_collection_keys =[i for i in data.keys()]
+            all_collection_values = [j for i,j in data.items()]
+            
+
+            def collec_func(collection_name):
+                global card_label, right_scroll, left_scroll, collec_len
+
+                collec_len = len(data[collection_name]) - 1
+
+                for i in all_labels:
+                    i.grid_forget()
+
+                for j in all_bts:
+                    j.grid_forget()
+
+                canvas.grid_forget()
+                collec_bt.grid_forget()
+                new_bt.grid_forget()
+                existing_bt.grid_forget()
+
+                back_bt = Button(text="Back",font=FONT, command=view_existing)
+                back_bt.grid(column=2, row=0,padx=20, pady=10)
+                all_bts.append(back_bt)
+
+                logo.config(file='front.png')
+                card_label = Label(text=data[collection_name][0]['title'], image=logo, compound="center", anchor="center", font=('times', 35, 'bold'))
+                card_label.config(fg="white")
+                card_label.grid(column=1, row=1)
+                all_labels.append(card_label)
+                
+                def right_skip(right_no):
+                    global left_scroll, right_scroll, card_label, collec_len, show_no, left_no
+                    card_label['text'] = data[collection_name][right_no ]['title']
+
+                    if left_no > 0:
+                        left_scroll.config(state=NORMAL)
+                    
+
+                    if collec_len > right_no:
+                        left_scroll.config(state=NORMAL)
+                        left_no += 1
+                        right_scroll.config(command=lambda:right_skip(right_no + 1))
+                        show_no += 1
+
+                    elif collec_len == right_no:
+                        show_no += 1
+                        left_no += 1
+                        right_scroll.config(state=DISABLED)
+
+                def left_skip(left_no):
+                    global left_scroll, right_scroll, card_label, collec_len, show_no, right_no
+                    
+                    if right_no >= 0:
+                        right_scroll.config(state=NORMAL)
+                    
+                    if  right_no < left_no:
+                        left_no -= 1
+                        right_scroll.config(state=NORMAL)        
+                        if left_no <= 0:
+                            left_scroll.config(state=DISABLED)
+                        card_label['text'] = data[collection_name][left_no]['title']
+                        # left_scroll.config(state=NORMAL)
+                        right_no -= 1
+                        left_scroll.config(command=lambda:left_skip(left_no))
+                        show_no -= 1
+                        
+                    
+
+                left_scroll = Button(text='<<<' , command=lambda:left_skip(left_no))
+                if collec_len == 0 and left_no == 0:
+                    left_scroll.config(state=DISABLED)
+                left_scroll.grid(column=0, row=2)
+                
+                all_bts.append(left_scroll)
+
+                right_scroll = Button(text='>>>', command=lambda:right_skip(right_no))
+                right_scroll.grid(column=2, row=2)
+                if collec_len == 0:
+                    right_scroll.config(state=DISABLED)
+                all_bts.append(right_scroll)
+
+                def show_cont(right_no):
+                    global left_scroll, right_scroll, card_label, collec_len
+                    card_label['text'] = data[collection_name][right_no ]['content']
+
+                show_bt = Button(text='Show', font=FONT, command=lambda:show_cont(show_no))
+                show_bt.grid(column=1, row=2, pady=20)
+                if collec_len == 0:
+                    show_bt.config(state=DISABLED)
+                all_bts.append(show_bt)
+
+            for _ in range (total_collection):
+                cols +=1
+                
+                if cols > 2:
+                    cols = 0
+                    rows += 1
+
+                collec_bt = Button(text=all_collection_keys[_], font=FONT, command= lambda k=_: collec_func(all_collection_keys[k]))
+                collec_bt.grid(row=rows, column=cols, padx=40, pady=20)
+                all_bts.append(collec_bt)
+                
+            
+        
+
     except:
         messagebox.showerror(title='FILE NOT FOUND', message="There no existing collection !")
 
@@ -115,15 +256,12 @@ canvas.grid(column=1, row=0)
 title = Label(text='Flash Cards', font=('TimesNewRoman', 45, 'bold'))
 title.grid(column=3, row=0)
 
-gap_ = Label(text='\t\t\t\n')
-gap_.grid(column=1, row=1)
-
 new_bt = Button(text='New', font=FONT, command=create_card)
 new_bt.config(padx=10)
-new_bt.grid(column=1, row=2, columnspan=2)
+new_bt.grid(column=1, row=1, columnspan=2, padx=15, pady=15)
 
 existing_bt = Button(text='Existing', font=FONT, command=view_existing)
-existing_bt.grid(column=2, row=2, columnspan=2)
+existing_bt.grid(column=2, row=1, columnspan=2, padx=15, pady=15)
 
 
 window.mainloop()
